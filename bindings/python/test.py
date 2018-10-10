40f0ba4d434fe8ac05118505e716817b4166322d
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2018-10-10 17:11:55 +0200

    Update test.py to use try ... catch

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -4,17 +4,18 @@
 from morfeusz2 import Morfeusz
 from concraft_pl2 import Concraft, Server
 
-morfeusz = Morfeusz(expand_tags=True)
-server = Server(model_path="/home/kuba/work/ipipan/concraft/pre-trained/Sep-18/model-04-09-2018.gz", port=3001)
-concraft = Concraft(port=3001)
-
-dag = morfeusz.analyse(u'W Szczebrzeszynie chrząszcz brzmi w trzcinie.')
-res = concraft.disamb(dag)
-print(res)
-
-dag = morfeusz.analyse(u'W Szczebrzeszynie chrząszcz brzmi w trzcinie.')
-dag_str = concraft.dag_to_str(dag)
-dag_disamb_str = concraft.disamb_str(dag_str)
-print(dag_disamb_str)
-
-server.terminate()
+try:
+  morfeusz = Morfeusz(expand_tags=True)
+  server = Server(model_path="/home/kuba/work/ipipan/concraft/pre-trained/Sep-18/model-04-09-2018.gz", port=3001)
+  concraft = Concraft(port=3001)
+  
+  dag = morfeusz.analyse(u'W Szczebrzeszynie chrząszcz brzmi w trzcinie.')
+  res = concraft.disamb(dag)
+  print(res)
+  
+  dag = morfeusz.analyse(u'W Szczebrzeszynie chrząszcz brzmi w trzcinie.')
+  dag_str = concraft.dag_to_str(dag)
+  dag_disamb_str = concraft.disamb_str(dag_str)
+  print(dag_disamb_str)
+finally:
+  server.terminate()
